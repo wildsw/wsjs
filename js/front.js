8dfd5ae27ddcdb4775eb3c48b7f5361b27868ea6
wildsw <wildsw@users.noreply.github.com>
2018-04-08 22:39:12 +0300

    Create front.js

---
+++ js/front.js
@@ -0,0 +1,11 @@
+var cat = document.querySelector("#orbiter");
+  var angle = 0, lastTime = null;
+  function animate(time) {
+    if (lastTime != null)
+      angle += (time - lastTime) * 0.001;
+    lastTime = time;
+    cat.style.top = 300+(Math.sin(angle) * 100) + "px";
+    cat.style.left = (Math.cos(angle) * 200) + "px";
+    requestAnimationFrame(animate);
+  }
+  requestAnimationFrame(animate);
